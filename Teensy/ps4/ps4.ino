94ae52fb6dd09efc01556e3e17b8b0d53fe17f1f
Anant Agarwal <anant22saawara@gmail.com>
2024-11-13 17:25:02 +0530

    Changed codes

--- Teensy/ps4/ps4.ino
+++ Teensy/ps4/ps4.ino
@@ -1,21 +1,23 @@
 #include <USBHost_t36.h>
 
 USBHost mysub;
-USBHIDParser hid1(mysub);
+JoystickController ps4(mysub);
 BluetoothController bt(mysub , true , "0000");
-JoystickController ps4(bt);
+USBHIDParser hid1(mysub);
 
 int prevLX=0;
 int prevLY=0;
 int prevRY=0;
 void setup(){
+
   Serial.begin(9600);
   mysub.begin();
 
-  bt.begin();
-
-  Serial.println("Connecting to PS4 controller...");
-
+  delay(5000);
+ 
+  Serial.println("Connecting to PS4 controller");
+  
+  delay(5000);
 }
 
 
@@ -25,17 +27,18 @@ void loop(){
   if(ps4.available()){
     int LX = ps4.getAxis(0);
     int LY = ps4.getAxis(1);
-    int RY = ps4.getAxis(3);
-    if(LX!=prevLX)
-      Serial.println(LX);
-    if(LY!=prevLY)
-      Serial.println(LY);
-    if(RY!=prevRY)
-      Serial.println(RY);
-    prevLX = LX;
-    prevLY = LY;
-    prevRY=RY;
+    int RX = ps4.getAxis(2);
+    int RY = ps4.getAxis(5);
+    Serial.print("Lx: ");
+    Serial.println(LX);
+    Serial.print("Ly: ");
+    Serial.println(LY);
+    Serial.print("Rx: ");
+    Serial.println(RX);
+    Serial.print("Ry: ");
+    Serial.println(RY);
   }
   else
     Serial.println("Not connected");
+  delay(100);
 }
